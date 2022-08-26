225927f29c520e59bdcd3c3473844b33bbaa8eec
siriuslatte <zeydiscorddev.business@gmail.com>
2022-08-26 01:55:23 -0500

    fix(banner text)

--- src/components/banner.js
+++ src/components/banner.js
@@ -6,7 +6,7 @@ export const Banner = () => {
 
             <div className="information">
                 <h1 className="aboutTitle">Ademir Sánchez</h1>
-                <p className="informationParagraph">Hello, I'm a 17yrs-old mexican TypeScript, Lua, Java, C# and Python Developer. I have over 4yrs of experience backing me up, with knowledge on frameworks like React, Three.js, Rodux, etc.</p>
+                <p className="informationParagraph">Hello, I'm a 17yrs-old mexican TypeScript and Luau Developer. I have over 4yrs of experience backing me up, I've been in and out of the ROBLOX Platform and currently learning at its fullest TypeScript.</p>
             </div>
 
             <Spline className="spline-rendering" scene="https://prod.spline.design/usgcpcSEIaQzlyS4/scene.splinecode" />
